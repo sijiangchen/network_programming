c21082ed75e7ca808528c322a6c03cb3597d3f0b
Elipzer <peterm8@rpi.edu>
2020-11-24 10:48:29 -0500

    Update hw4_controller.py

--- hw4_controller.py
+++ hw4_controller.py
@@ -1,5 +1,32 @@
 import sys
 
+class BaseStation:
+    def __init__(self, baseId, x, y, links):
+        self.id = baseId
+        self.x = x
+        self.y = y
+        self.links = links
+    
+    @staticmethod
+    def create(line):
+        line = line.split(' ')
+        return Client(line[0], line[1], line[2], line[4:])
+
+    @staticmethod
+    def createAll(path):
+        l = []
+        with open(path) as f:
+            for line in f:
+                line = line.strip()
+                if line == "":
+                    continue
+                l.append(Client.create(line))
+        return l
+
+
+    def __str__(self):
+        return "BaseStation::<id: {}, x: {}, y: {}, links: [{}]>".format(self.baseId, self.x, self.y, ", ".join(self.links))
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print('wrong')
@@ -7,3 +34,7 @@ if __name__ == "__main__":
     controlPort = sys.argv[1]
     baseStationFile = sys.argv[2]
     print(controlPort, baseStationFile)
+
+    baseStations = BaseStation.createAll("base_stations.txt")
+    for baseStation in baseStations:
+        print(baseStation)
